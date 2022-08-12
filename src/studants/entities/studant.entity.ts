ec4307b4e9698eb8a855b91e3cdbba6f4f752bc1
Danilo Alves de Aguiar <daniloalves0512@gmail.com>
2022-08-12 19:21:45 -0300

    fix: alteration in (.env) and correction (studant.entity)

--- src/studants/entities/studant.entity.ts
+++ src/studants/entities/studant.entity.ts
@@ -1,11 +1,12 @@
 import { Consult } from "src/consult/entities/consult.entity";
+import { Institution } from "src/institution/entities/institution.entity";
 
 export class Studant {
   id?: string;
   nome: string;
   data_nasc: string;
   telefone?: string;
-  instituicaoId: string;
+  instituicao?: Institution[];
   createdAt?: Date;
   updatedAt?: Date;
   consultas?: Consult[];
