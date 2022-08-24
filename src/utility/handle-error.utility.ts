c1f06547aaaf441ad50050364e4bab5094eb4f4b
Alex <97993854+AlexFariaa@users.noreply.github.com>
2022-08-24 19:22:54 -0300

    feat(utility): update handle-error

--- src/utility/handle-error.utility.ts
+++ src/utility/handle-error.utility.ts
@@ -1,4 +1,4 @@
-import { UnprocessableEntityException } from '@nestjs/common';
+import { BadRequestException, InternalServerErrorException, NotFoundException, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 
 export function handleError(err: Error): undefined {
   const error = err.message?.split('\n');
@@ -11,4 +11,61 @@ export function handleError(err: Error): undefined {
   throw new UnprocessableEntityException(
     lastErrLine || 'Algum erro ocorreu ao executar a operação',
   );
+
+}
+
+export function BadRequest(err: Error): undefined {
+  const error = err.message?.split('\n');
+  const lastErrLine = error[error.length - 1]?.trim();
+
+  if (!lastErrLine) {
+    console.error(err);
+  }
+
+  throw new BadRequestException(
+    lastErrLine || 'Algum erro ocorreu ao executar a operação',
+  );
+
+}
+
+export function Unauthorized(err: Error): undefined {
+  const error = err.message?.split('\n');
+  const lastErrLine = error[error.length - 1]?.trim();
+
+  if (!lastErrLine) {
+    console.error(err);
+  }
+
+  throw new UnauthorizedException(
+    lastErrLine || 'Algum erro ocorreu ao executar a operação',
+  );
+
+}
+
+export function NotFound(err: Error): undefined {
+  const error = err.message?.split('\n');
+  const lastErrLine = error[error.length - 1]?.trim();
+
+  if (!lastErrLine) {
+    console.error(err);
+  }
+
+  throw new NotFoundException(
+    lastErrLine || 'Algum erro ocorreu ao executar a operação',
+  );
+
+}
+
+export function ServerError(err: Error): undefined {
+  const error = err.message?.split('\n');
+  const lastErrLine = error[error.length - 1]?.trim();
+
+  if (!lastErrLine) {
+    console.error(err);
+  }
+
+  throw new InternalServerErrorException(
+    lastErrLine || 'Algum erro ocorreu ao executar a operação',
+  );
+
 }
